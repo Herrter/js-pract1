030dcc6d433c8d8953a9e2b104efad6e57d5ab1f
Herrter <113292832+Herrter@users.noreply.github.com>
2022-11-01 18:13:55 +0200

    Update HW.js

--- HW.js
+++ HW.js
@@ -1,3 +1,7 @@
+// - Створити змінні. 
+// Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false. 
+// Вивести кожну змінну за допомогою: console.log 
+
 let t1 = `hello`;
 console.log (t1);
 t1 = `owu`;
@@ -26,11 +30,17 @@ t1 = false;
 console.log (t1);
 
 
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
 let firstname = `Lomaka`;
 let middlename = `Mykyta`;
 let lastname = `Sergiyovych`;
 console.log(`${firstname} ${middlename} ${lastname}`);
 
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
 
 let a = 100
 let b = `100`
@@ -38,6 +48,10 @@ let c = true
 console.log(typeof a)
 console.log(typeof b)
 console.log(typeof c)
+
+
+// Додаткове для тих хто цікавився prompt`oм За допомогою 3х різних prompt() отримати 3 слова які являються вашими
+// Імям, По-Батькові та роками. та вивести в консоль
 
 let years = prompt('Сколько вам лет?', );
 console.log(years)
