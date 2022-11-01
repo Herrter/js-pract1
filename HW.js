d40b9d52f719015a9b5936ee110da4c9ac9166f0
Herrter <113292832+Herrter@users.noreply.github.com>
2022-11-01 17:39:03 +0200

    Add files via upload

---
+++ HW.js
@@ -0,0 +1,47 @@
+let t1 = `hello`;
+console.log (t1);
+t1 = `owu`;
+console.log (t1);
+t1 = `com`;
+console.log (t1);
+t1 = `ua`;
+console.log (t1);
+t1 = 1;
+console.log (t1);
+t1 = 10;
+console.log (t1);
+t1 = -999;
+console.log (t1);
+t1 = 123;
+console.log (t1);
+t1 = 3.14;
+console.log (t1);
+t1 = 2.7;
+console.log (t1);
+t1 = 16;
+console.log (t1);
+t1 = true;
+console.log (t1);
+t1 = false;
+console.log (t1);
+
+
+let firstname = `Lomaka`;
+let middlename = `Mykyta`;
+let lastname = `Sergiyovych`;
+console.log(`${firstname} ${middlename} ${lastname}`);
+
+
+let a = 100
+let b = `100`
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+let years = prompt('Сколько вам лет?', );
+console.log(years)
+let names = prompt('Ваше имя и фамилия?', );
+console.log(names)
+let lastnames = prompt('По отчеству?', );
+console.log(lastnames)
